f953e2e210f6f323f5d8b58ff2bb96b27abfb266
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2018-02-08 12:00:06 +0300

    start working on linked list

--- src/single-linked-list.js
+++ src/single-linked-list.js
@@ -2,6 +2,28 @@ class SingleLinkedList {
     constructor() {
         this.tail = null;
         this.head = null;
+        this.length = 0;
+    }
+
+    push() {
+        Array.from(arguments).forEach((value, index) => {
+            const addingNode = new Node(value);
+            if (this.head) {
+                let current = this.head;
+                while (current) {
+                    current = current.next;
+                }
+            } else {
+                this.head = addingNode;
+            }
+        })
+    }
+}
+
+class Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
     }
 }
 
